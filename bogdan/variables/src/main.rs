09461ec0210b571ebda483fd2e9ff5c31054111c
Bogdan <bogdan.oloeriu@gmail.com>
2021-12-28 10:56:30 +0100

    exercise b complete

---
+++ bogdan/variables/src/main.rs
@@ -0,0 +1,7 @@
+const STARTING_MISSILES: i32 = 9;
+const READY_AMOUNT: i32 = 2;
+fn main() {
+    let mut _missiles = STARTING_MISSILES;
+    let _ready = READY_AMOUNT;
+    println!("Firing {} of my {} missiles...!", _ready, _missiles);
+}
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,13 @@
+use hello::greet;
+
 fn main() {
     println!("Hello, world!");
+    let _bunnies: i32 = 4;
+    let (_bunnies_2, _carrots) = (4, 5);
+
+    let _x = 5;
+    {
+        let _x = 99;
+    }
+    greet();
 }
